23b6037cfc78d19622ee3b8720d49d34bf614364
Prakash Tiwari <prakashtiwari.net@gmail.com>
2022-08-02 20:52:15 +0530

    Parallel processing

---
+++ CuncuParallel/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CuncuParallel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DownloadFileOne();
+            DownloadFileTwo();
+            //Task.Factory.StartNew(DownloadFileThree);
+            //Task.Factory.StartNew(DownloadFileFour);
+
+            string s = "";// Console.ReadLine();
+            Console.WriteLine("User entered: " + s);
+            Console.ReadLine();
+        }
+        static async void DownloadFileOne()
+        {
+            await Task.Delay(10000);
+            Console.WriteLine("Downloading file1!");
+        }
+        static async void DownloadFileTwo()
+        {
+            await Task.Delay(10000);
+            Console.WriteLine("Downloading file2!");
+
+        }
+        static  void DownloadFileThree()
+        {
+             Task.Delay(10000);
+            Console.WriteLine("Downloading file3!");
+        }
+        static  void DownloadFileFour()
+        {
+             Task.Delay(10000);
+            Console.WriteLine("Downloading file4!");
+        }
+    }
+}
